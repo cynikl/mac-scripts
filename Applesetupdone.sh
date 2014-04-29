0c2c4d73ef48e36c0686fcfb10fc9784f664ebc0
Cyril Niklaus <cynik@mac.com>
2014-04-29 07:51:02 +0200

    Added permissions

--- Applesetupdone.sh
+++ Applesetupdone.sh
@@ -3,12 +3,8 @@
 # cyril.niklaus@gmail.com
 # In order to get rid of the assistant when Deploying images done with AutoDMG,
 # it makes more sense to use that functionnality in DSS.
-# v 0.2 2014-04-29 Added .SetupRegComplete and permissions
+# v 0.2 2014-04-29 Added permissions
 
 touch /var/db/.AppleSetupDone
 chmown root:wheel /var/db/.Applesetupdone
 chmod 600 /var/db/.Applesetupdone
-
-touch /Library/Receipts/.SetupRegComplete
-chown root:wheel /Library/Receipts/.SetupRegComplete
-chmod	400 /Library/Receipts/.SetupRegComplete
