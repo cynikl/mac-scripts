4a865e1e73d5b82187476d7deac7688b8e488c14
Cyril Niklaus <cynik@mac.com>
2014-05-05 12:00:16 +0200

    updates to scripts

---
+++ firstsetup/disablelocal_snapshots.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Disable local TimeMachine snapshots 
+# They are off by default on desktop computers, but on on laptops.
+# to get them back on: tmutil enablelocal
+# Cyril Niklaus
+tmutil disablelocal
