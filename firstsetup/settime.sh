4f63fb389fc471dfeb33574bbc5188353788a513
Cyril Niklaus <cynik@mac.com>
2014-05-05 18:07:01 +0200

    cleanup

--- firstsetup/settime.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# set the time zone
-/usr/sbin/systemsetup -settimezone Europe/Zurich
-/usr/sbin/systemsetup -setusingnetworktime on
-/usr/sbin/systemsetup -setnetworktimeserver time.euro.apple.com
