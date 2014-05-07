05acb78201b32adfe6d42964ce57ca6254e849d7
Cyril Niklaus <cynik@mac.com>
2014-05-07 22:48:39 +0200

    added ntpdate

--- firstsetup/settime.sh
+++ firstsetup/settime.sh
@@ -3,3 +3,7 @@
 /usr/sbin/systemsetup -settimezone Europe/Zurich
 /usr/sbin/systemsetup -setusingnetworktime on
 /usr/sbin/systemsetup -setnetworktimeserver time.euro.apple.com
+
+# update the time
+/usr/sbin/ntpdate -bs time.euro.apple.com
+exit 0
