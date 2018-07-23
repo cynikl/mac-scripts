584fbd4fe2d18585b2ac595da929c4b042e4a271
Cyril Niklaus <cynik@mac.com>
2018-07-23 12:01:39 +0200

    logging

--- log_with_timestamp.sh
+++ log_with_timestamp.sh
@@ -1,13 +1,37 @@
-#!/bin/bash
-# log date format
-DATE='date +%Y-%m-%d_%H:%M:%S'
-# log file
-LOG='/Users/cyril/setidentity.log'
+	#!/bin/bash
+	# Cyril Niklaus at amaris dot com
+	# logging function and timing
+	# 2018-07
+	
+	# date string
+	DATE=$(/bin/date "+%Y-%m-%d %H:%M:%S")
+	# logfile location and creation if necessary
+	logFile="$HOME/test.log"
+	if [ ! -f "$logFile" ];
+	then
+	/usr/bin/touch ${logFile}
+	fi
 
-function echo_log {
-	echo `$DATE`" $1" >> $LOG
-}
-# start
-echo_log "This is the start."
-
-exit 0
+	function echo_log {
+		/bin/echo "$DATE" " $1" >> ${logFile}
+	}
+	# 
+	# start chrono
+	startChrono=$(/bin/date '+%s')
+	
+	###
+	# This is the main part of the script, to be modified
+	# Do stuff:
+	echo_log "This is a test 2"
+	ls /Applications
+	sleep 3
+	echo_log " line 3"
+	# end of main part of the script
+	###
+	
+	# stop chrono, calculate the difference and log it
+	stopChrono=$(/bin/date '+%s')
+	timeElapsed=$(($stopChrono-$startChrono))
+	echo_log "This script took $timeElapsed seconds to run."
+	
+	exit 0
